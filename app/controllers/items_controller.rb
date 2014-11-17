75583326f3d82de07d8d6c9b2512ab1225ce65e3
Logan Rice <loganrice72@gmail.com>
2014-11-17 11:51:34 -0800

    formatted item index table

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
   
-    def index
-      @items = Item.all 
+  def index
+    @items = Item.all
+    respond_to do |format|
+      format.js
     end
+  end
 end
