f65ecae03754b7a008c3d522419b34ab66c3b3e7
Logan Rice <loganrice72@gmail.com>
2014-11-26 15:39:21 -0800

    can create an initial contract

--- app/controllers/contracts_controller.rb
+++ app/controllers/contracts_controller.rb
@@ -8,4 +8,17 @@ class ContractsController < ApplicationController
   def new
     @contract = Contract.new
   end
+
+  def create
+    @contract = Contract.new(contract_params)
+    @contract.save
+    flash[:success] = "Updated"
+    redirect_to contracts_path
+  end
+
+  private
+
+  def contract_params
+    params.require(:contract).permit(:buyer_contract, :buyer_po, :seller_contract, :date)
+  end
 end
--- spec/controllers/contracts_controller_spec.rb
+++ spec/controllers/contracts_controller_spec.rb
@@ -16,4 +16,21 @@ describe ContractsController do
       expect(assigns(:contract)).to be_instance_of(Contract)
     end
   end
+
+  describe "POST create" do
+    context "with valid input" do 
+      it "creates a contract" do 
+        xhr :post, :create, contract: { buyer_contract: '1234', buyer_po: '23432', seller_contract: 'aa1345', date: Time.new.to_date }
+        expect(Contract.count).to eq(1)
+      end
+      it "sets the flash success message" do 
+        xhr :post, :create, contract: { buyer_contract: '1234', buyer_po: '23432', seller_contract: 'aa1345', date: Time.new.to_date }
+        expect(flash[:success]).to be_present
+      end
+      it "redirects to the contracts path" do 
+        xhr :post, :create, contract: { buyer_contract: '1234', buyer_po: '23432', seller_contract: 'aa1345', date: Time.new.to_date }
+        expect(response).to redirect_to contracts_path
+      end
+    end
+  end
 end
