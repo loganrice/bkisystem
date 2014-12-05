8606458908d36785859e3965b0299f00a23458eb
Logan Rice <loganrice72@gmail.com>
2014-12-05 11:45:14 -0800

    login template view

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'contracts#home'
+  root to: 'contracts#home'
   resources :contracts
   resources :items
   resources :commodities
   resources :sizes
   resources :varieties
   resources :accounts
+  get 'login' => 'pages#login' 
   get 'ui(/:action)', controller: 'ui'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
