b6594a3ac4a27385ca4ca769ae71eb24516b3654
Logan Rice <loganrice72@gmail.com>
2014-12-17 09:17:17 -0800

    quote line item methods to convert cents to dollars

--- app/models/quote_line_item.rb
+++ app/models/quote_line_item.rb
@@ -2,4 +2,13 @@ class QuoteLineItem < ActiveRecord::Base
   belongs_to :quote
   belongs_to :item
   has_many :order_line_items
+
+  def price_dollars=(dollars)
+    self.price_cents = dollars * 100
+  end
+
+  def price_dollars
+    self.price_cents.to_f / 100
+  end
+
 end
--- spec/models/quote_line_item_spec.rb
+++ spec/models/quote_line_item_spec.rb
@@ -4,4 +4,20 @@ describe QuoteLineItem do
   it { should belong_to(:quote) }
   it { should belong_to(:item) }
   it { should have_many(:order_line_items)}
+
+  describe "#price_dollars=" do
+    it "converts $12.93 dollars to 1293 cents" do 
+      amount = QuoteLineItem.create()
+      amount.price_dollars = 12.93
+      expect(amount.price_cents).to eq(1293)
+    end
+  end
+
+  describe "#price_dollars" do 
+    it "displays 1293 cents as 12.93" do 
+      amount = QuoteLineItem.create(price_cents: 1293)
+      expect(amount.price_dollars).to eq(12.93)
+    end
+  end
+
 end
