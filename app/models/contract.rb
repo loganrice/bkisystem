7212c88895cdae2ca336872d26ee2a5c82447395
Logan Rice <loganrice72@gmail.com>
2014-12-22 11:24:26 -0800

    fixed contract call back to create quote on create

--- app/models/contract.rb
+++ app/models/contract.rb
@@ -6,36 +6,12 @@ class Contract < ActiveRecord::Base
   has_many :quote_line_items, through: :quote
   accepts_nested_attributes_for :orders, allow_destroy: true
   accepts_nested_attributes_for :quote, allow_destroy: true
-  after_save :update_order_line_items_from_quote_line_items
-  before_save :set_default_quote_object
+  before_create :create_quote
 
   private 
 
-  def set_default_quote_object
+  def create_quote
     self.quote ||= Quote.create()
   end
 
-  def update_order_line_items_from_quote_line_items
-    self.orders.each do |order|
-      order.order_line_items.each do |line_item|
-        update_order_item(line_item)
-      end
-    end
-  end
-
-  def update_order_item(line_item)
-    quote = get_quote(line_item)
-    item = get_item_from_quote(quote)
-    line_item.item = item 
-  end
-
-  def get_quote(order_line)
-    id = order_line.quote_line_item_id 
-    QuoteLineItem.find(id)
-  end
-
-  def get_item_from_quote(quote_line)
-    id = quote_line.item_id
-    Item.find(id) 
-  end
 end
--- spec/models/contract_spec.rb
+++ spec/models/contract_spec.rb
@@ -8,29 +8,10 @@ describe Contract do
   it { should have_many(:orders) }
   it { should accept_nested_attributes_for(:orders) }
 
-  describe "#update_order_line_item_with_quote_line_item" do 
-    it "updates the order_line_item_price with the quote price"
-    it "updates the order_line_item.item_id with the quote item" do 
-      contract = Contract.create()
-      quote = Fabricate(:quote)
-      contract.quote = quote
-      item = Fabricate(:item)
-
-      quote_line_item = Fabricate(:quote_line_item, price_cents: 100, item_id: item.id)
-      order_line_item = Fabricate(:order_line_item, quote_line_item_id: quote_line_item.id, item_id: nil)
-      
-      quote.quote_line_items << quote_line_item
-
-      order = Fabricate(:order)
-      order.order_line_items << order_line_item
-      contract.orders << order
-      
-      contract.save
-      contract.reload
-      first_line_on_first_order = Contract.first.orders.first.order_line_items.first
-      expect(first_line_on_first_order.item_id).to eq(quote_line_item.item_id)
-
-    end
+  it "creates a quote before savinging if one has not already been created" do 
+    contract = Contract.create()
+    expect(contract.quote).to be_instance_of(Quote)
   end
+
 end
 
