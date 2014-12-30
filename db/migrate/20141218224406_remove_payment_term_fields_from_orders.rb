1a9635d744559ccd65869e3f951cd956a67760c6
Logan Rice <loganrice72@gmail.com>
2014-12-30 15:34:35 -0800

    migration error column does not exist

--- db/migrate/20141218224406_remove_payment_term_fields_from_orders.rb
+++ db/migrate/20141218224406_remove_payment_term_fields_from_orders.rb
@@ -1,5 +1,7 @@
 class RemovePaymentTermFieldsFromOrders < ActiveRecord::Migration
   def change
-    remove_column :order_line_items, :payment_terms
+    # remove_column :order_line_items, :payment_terms
+    # commented how due to migration error
+    # column "payment_terms" of relation "order_line_items" does not exist
   end
 end
