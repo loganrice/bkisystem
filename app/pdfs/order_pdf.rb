60bb8c7fc49bc9eb18631f0ca85abd8e322062d4
Logan Rice <loganrice72@gmail.com>
2015-01-26 09:42:23 -0800

    contract report up to addresses

--- app/pdfs/order_pdf.rb
+++ app/pdfs/order_pdf.rb
@@ -9,31 +9,22 @@ class OrderPdf
   end
 
   def create_contract_report
-    define_grid(columns: 5, rows: 12, gutter: 10)
+    stroke_axis
+
     header
     move_down 20
     contract_number
-    # seller_buyer_name_address
-
-    grid.show_all
+    move_down 20
+    seller_address
+    # buyer_address
+    # grid.show_all
 
   end
 
   def header
-    line [0, 650], [650, 650]
-    stroke
-    stroke_axis
-    bounding_box([cursor, cursor], width: 200) do 
-      text "alskdfjasdf adslfjasdfkjasfd asldfijasdflkjasdf asldfjasldkfj asdflkj"
-    end
-    grid([0,0],[0,4]).bounding_box do
-      text "Sales Contract", size: 30, style: :bold, align: :center, valign: :bottom
-    end
-
-    grid(0,0).bounding_box do 
-      image "#{Rails.root}/app/assets/images/bki_logo.png", :valign => :bottom
-    end
-
+    image "#{Rails.root}/app/assets/images/bki_logo.png", at: [0, cursor]
+    text "Sales Contract", size: 30, style: :bold, align: :center
+    stroke_horizontal_rule
   end
 
   def contract_number
@@ -42,15 +33,82 @@ class OrderPdf
     text "<b>Customer Contract No:</b> #{@order.contract.buyer_contract}", inline_format: true
   end
 
-  def seller_buyer_name_address
-    text "SELLER"
-    grid(1,3).bounding_box do
-      text "BUYER"
+  def seller_address
+    text_box "<b><u>SELLER</u></b>", inline_format: true, at: [0, 600]
+    text_box "<b><u>BUYER</u></b>", inline_format: true, at: [300, 600]
+
+    move_down 15
+    buyer_y_base = cursor 
+    seller_y_base = cursor
+
+    text_box @order.contract.seller.name, inline_format: true, at: [0, cursor]
+    text_box @order.contract.buyer.name, inline_format: true, at: [300, buyer_y_base]
+
+    buyer_y_base -= 15
+    seller_y_base -= 15
+
+    unless @order.contract.seller.addresses.first.line1.blank?
+      text_box @order.contract.seller.addresses.first.line1, inline_format: true, at: [0, seller_y_base]
+      seller_y_base -= 15
     end
-    grid(6,3).bounding_box do
-      text "SOMETHING"
+
+    unless @order.contract.buyer.addresses.first.line1.blank?
+      text_box @order.contract.buyer.addresses.first.line1, inline_format: true, at: [300, buyer_y_base]
+      buyer_y_base -= 15
     end
-    grid.show_all
+    
+    unless @order.contract.seller.addresses.first.line2.blank?
+      text_box @order.contract.seller.addresses.first.line2, inline_format: true, at: [0, seller_y_base]
+      seller_y_base -= 15 
+    end
+
+    unless @order.contract.buyer.addresses.first.line2.blank?
+      text_box @order.contract.buyer.addresses.first.line2, inline_format: true, at: [300, buyer_y_base]
+      buyer_y_base -= 15   
+    end
+
+    move_down 15
+
+    unless @order.contract.seller.addresses.first.line3.blank?
+      text_box @order.contract.seller.addresses.first.line3, inline_format: true, at: [0, seller_y_base]
+      seller_y_base -= 15
+    end
+
+    unless @order.contract.buyer.addresses.first.line3.blank?
+      text_box @order.contract.buyer.addresses.first.line3, inline_format: true, at: [300, buyer_y_base]
+      buyer_y_base -= 15
+    end
+
+    text_box "#{@order.contract.seller.addresses.first.city} " +
+             "#{@order.contract.seller.addresses.first.state} " +
+             "#{@order.contract.seller.addresses.first.zip}", inline_format: true, at: [0, seller_y_base]
+    text_box "#{@order.contract.buyer.addresses.first.city} " +
+             "#{@order.contract.buyer.addresses.first.state} " +
+             "#{@order.contract.buyer.addresses.first.zip}", inline_format: true, at: [300, buyer_y_base]
+  end
+
+  def buyer_address
+    text_box "<b><u>BUYER</u></b>", inline_format: true, at: [300, 600]
+    move_down 15
+
+    text_box @order.contract.buyer.name, inline_format: true, at: [300, cursor]
+    move_down 15
+
+    text_box @order.contract.buyer.addresses.first.line1, inline_format: true, at: [300, cursor]
+
+    unless @order.contract.buyer.addresses.first.line2.blank?
+      move_down 15
+      text_box @order.contract.buyer.addresses.first.line2, inline_format: true, at: [300, cursor]
+    end
+
+    unless @order.contract.buyer.addresses.first.line3.blank?
+      move_down 15
+      text_box @order.contract.buyer.addresses.first.line3, inline_format: true, at: [300, cursor]
+    end
+    move_down 15
+    text_box "#{@order.contract.buyer.addresses.first.city} " +
+             "#{@order.contract.buyer.addresses.first.state} " +
+             "#{@order.contract.buyer.addresses.first.zip}", inline_format: true, at: [300, cursor]
   end
 
   def line_item_rows
