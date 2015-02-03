a73aa37e950bc82f479d9b8d485281df8dcb941c
Logan Rice <loganrice72@gmail.com>
2015-02-03 10:59:46 -0800

    validate presence of name on commodity table

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,5 @@
 class Grade < ActiveRecord::Base
   has_many :items
+  validates_presence_of :name
+  
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -2,4 +2,6 @@ require 'spec_helper'
 
 describe Grade do
   it { should have_many(:items) } 
+  it { should validate_presence_of(:name) }
+  
 end
