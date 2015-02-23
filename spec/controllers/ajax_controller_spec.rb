a5a368030ba5522fc0924080077d9e79ec45daab
Logan Rice <loganrice72@gmail.com>
2015-02-23 10:49:09 -0800

    item autocomplete ajax test

--- spec/controllers/ajax_controller_spec.rb
+++ spec/controllers/ajax_controller_spec.rb
@@ -4,8 +4,13 @@ describe AjaxController do
   describe 'GET item_names' do 
     it "returns a json list"
     it "searches for items matching terms" do 
-      get :item_names
-      expect(response).to eq([])
+      butte1 = Fabricate(:item, name: "Butte 1")
+      butte2 = Fabricate(:item, name: "Butte 2") 
+      nonpareil = Fabricate(:item, name: "nonpareil")
+      get :item_names, term: 'but'
+      body = JSON.parse(response.body)
+
+      body.should have(2).items
     end
   end
 end
