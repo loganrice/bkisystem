1e4ff2e6c4912ad1937ccf2146a76743db4967d8
Logan Rice <loganrice72@gmail.com>
2015-02-24 17:06:01 -0800

    altered migration for heroku error

--- db/migrate/20150222230731_change_show_bank_wire_info_on_invoices.rb
+++ db/migrate/20150222230731_change_show_bank_wire_info_on_invoices.rb
@@ -1,10 +1,13 @@
 class ChangeShowBankWireInfoOnInvoices < ActiveRecord::Migration
-  def up
-    change_column :invoices, :show_bank_wire_info, :boolean
+  # def up
+  #   change_column :invoices, :show_bank_wire_info, :boolean
 
-  end
+  # end
 
-  def down
-    change_column :invoices, :show_bank_wire_info, :string
-  end
+  # def down
+  #   change_column :invoices, :show_bank_wire_info, :string
+  # end
+
+  remove_column :invoices, :show_bank_wire_info
+  add_column :invoices, :show_bank_wire_info, :boolean
 end
