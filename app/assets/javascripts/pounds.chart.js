1e0b3e0ab324742cff5ad159f595475cd3b39b24
Logan Rice <loganrice72@gmail.com>
2015-03-13 14:00:01 -0700

    dashboard work in progress

--- app/assets/javascripts/pounds.chart.js
+++ app/assets/javascripts/pounds.chart.js
@@ -1,25 +1,23 @@
-
-
-// var poundsChart = Morris.Line({
+var poundsChart = Morris.Line({
 //     // ID of the element in which to draw the chart.
-//     element: 'pounds-by-month-chart',
+     element: 'pounds-by-month-chart',
 //     // Chart data records -- each entry in this array corresponds to a point on
 //     // the chart.
-//     data: $('#pounds-by-month-chart').data('pounds'),
+     data: $('#pounds-by-month-chart').data('pounds'),
 //     // The name of the data record attribute that contains x-values.
-//     xkey: 'date',
+     xkey: 'date',
 //     // A list of names of data record attributes that contain y-values.
-//     ykeys: ['pounds'],
+     ykeys: ['pounds'],
 //     // Labels for the ykeys -- will be displayed when you hover over the
 //     // chart.
-//     xLabels: 'month',
-//     labels: ['Total Pounds']
-// });
+     xLabels: 'month',
+     labels: ['Total Pounds']
+});
 
 
-// $(".easy-pie-chart").easyPieChart({
-//     animate: "true",
-//     barColor: "#337ab7"
-// });
+$(".easy-pie-chart").easyPieChart({
+   animate: "true",
+   barColor: "#337ab7"
+});
 
-// $('.sparkline').sparkline(); 
+$('.sparkline').sparkline(); 
